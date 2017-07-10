aa95543ce0d4ac1a0dfe61f8048aba4a3fd0d09f
731614105013 <gomathiammupk29@gmail.com>
2017-07-10 17:14:23 +0530

    Create StringInt.java

---
+++ StringInt.java
@@ -0,0 +1,13 @@
+# stringint
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class StringInt{
+  public static  void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    String s="10";
+    int a=Integer.parseInt(s);            
+    System.out.println(""+a);
+    }
+  } 
